abf96ade50e8bd69773483d93484aacb2059c35f
ro-vezur <authenttesting@gmail.com>
2024-11-16 21:09:18 +0200

    feat : states

---
+++ app/src/main/java/com/example/jetfilms/states/rememberForeverScrollState.kt
@@ -0,0 +1,35 @@
+package com.example.jetfilms.states
+
+import androidx.compose.foundation.ScrollState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.saveable.rememberSaveable
+
+private val SaveMap = mutableMapOf<String, ScrollKeyParams>()
+
+private data class ScrollKeyParams(
+    val value: Int
+)
+
+/**
+ * Save scroll state on all time.
+ * @param key value for comparing screen
+ * @param initial see [ScrollState.value]
+ */
+@Composable
+fun rememberForeverScrollState(
+    key: String,
+    initial: Int = 0
+): ScrollState {
+    val scrollState = rememberSaveable(saver = ScrollState.Saver) {
+        val scrollValue: Int = SaveMap[key]?.value ?: initial
+        SaveMap[key] = ScrollKeyParams(scrollValue)
+        return@rememberSaveable ScrollState(scrollValue)
+    }
+    DisposableEffect(Unit) {
+        onDispose {
+            SaveMap[key] = ScrollKeyParams(scrollState.value)
+        }
+    }
+    return scrollState
+}
