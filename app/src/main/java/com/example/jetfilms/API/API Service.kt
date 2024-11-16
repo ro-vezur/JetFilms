457182d4f9b74d49412f167db3de0d98b05fa31e
ro-vezur <authenttesting@gmail.com>
2024-11-16 21:05:26 +0200

    feat : API Service

--- app/src/main/java/com/example/jetfilms/API/API Service.kt
+++ app/src/main/java/com/example/jetfilms/API/API Service.kt
@@ -1,9 +1,11 @@
 package com.example.jetfilms.API
 
 import com.example.jetfilms.APIKEY
-import com.example.jetfilms.Data_Classes.DetailedMovieDataClass
-import com.example.jetfilms.Data_Classes.SimplifiedMovieDataClass
-import com.example.jetfilms.Data_Classes.moviesPageDataClass
+import com.example.jetfilms.Data_Classes.MoviePackage.DetailedMovieDataClassResponse
+import com.example.jetfilms.Data_Classes.ParticipantPackage.MovieCreditsDataClass
+import com.example.jetfilms.Data_Classes.MoviePackage.imagesFromTheMovieResponse
+import com.example.jetfilms.Data_Classes.MoviePackage.MoviesResponse
+import com.example.jetfilms.Data_Classes.ParticipantPackage.DetailedMovieParticipantResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -14,17 +16,47 @@ interface ApiInterface {
     suspend fun popularMovies(
         @Query("page") page: Int,
         @Query("api_key") api_key: String = APIKEY,
-    ):Response<moviesPageDataClass>
+    ): MoviesResponse
 
     @GET("movie/top_rated?")
     suspend fun topRatedMovies(
+        @Query("page") page: Int,
         @Query("api_key") api_key: String = APIKEY
-    ):Response<moviesPageDataClass>
+    ): MoviesResponse
 
     @GET("movie/{movie_id}")
     suspend fun movie(
         @Path("movie_id") movieId: Int,
         @Query("api_key") api_key: String = APIKEY,
+    ): Response<DetailedMovieDataClassResponse>
 
-    ):Response<DetailedMovieDataClass>
+    @GET("movie/{movie_id}/credits")
+    suspend fun movieCredits(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") api_key: String = APIKEY,
+    ): MovieCreditsDataClass
+
+    @GET("movie/{movie_id}/images")
+    suspend fun imagesFromMovie(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") api_key: String = APIKEY,
+    ): imagesFromTheMovieResponse
+
+    @GET("movie/{movie_id}/similar")
+    suspend fun similarMovies(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = APIKEY
+    ): MoviesResponse
+
+    @GET("person/{person_id}")
+    suspend fun participant(
+        @Path("person_id") personId: Int,
+        @Query("api_key") apiKey: String = APIKEY
+    ): DetailedMovieParticipantResponse
+
+    @GET("person/{person_id}/movie_credits")
+    suspend fun participantFilmography(
+        @Path("person_id") personId: Int,
+        @Query("api_key") apiKey: String = APIKEY
+    )
 }
