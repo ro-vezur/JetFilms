d51b94eb568b15911498f4b8b89561371808e093
ro-vezur <authenttesting@gmail.com>
2024-11-17 11:10:19 +0200

    fix: renamed/deleted files

--- app/src/main/java/com/example/jetfilms/Lazy_Column_Row_extensions/scroll offset.kt
+++
@@ -1,8 +0,0 @@
-package com.example.jetfilms.Lazy_Column_Row_extensions
-
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.State
-import androidx.compose.runtime.remember
-
