4b367197ba6c12a4e0bbc33a2a247d7d1f8308d7
ro-vezur <authenttesting@gmail.com>
2024-11-17 11:08:29 +0200

    feat: constants

--- app/src/main/java/com/example/jetfilms/Constants.kt
+++ app/src/main/java/com/example/jetfilms/Constants.kt
@@ -11,8 +11,10 @@ const val baseButtonHeight = 40
 const val bottomNavBarHeight = 54
 const val bottomNavItemSize = 28
 
-val blueGradient = Brush.horizontalGradient(listOf(buttonsColor1, buttonsColor2))
-val whiteGradient = Brush.horizontalGradient(listOf(Color.White.copy(0.82f), Color.White.copy(0.82f)))
+val blueHorizontalGradient = Brush.horizontalGradient(listOf(buttonsColor1, buttonsColor2))
+val blueVerticalGradient = Brush.horizontalGradient(listOf(buttonsColor1, buttonsColor2))
+
+const val PAGESIZE = 20
 
 const val baseAPIUrl = "https://api.themoviedb.org/3/"
 const val baseImageUrl = "https://image.tmdb.org/t/p/w500"
