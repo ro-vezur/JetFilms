6006526ad5f6d590feb2a780fdf8838d82f97b3a
ro-vezur <authenttesting@gmail.com>
2024-11-17 11:00:51 +0200

    feat: gradients

---
+++ app/src/main/java/com/example/jetfilms/CustomComposables/Gradient/Gradient with animation.kt
@@ -0,0 +1,23 @@
+package com.example.jetfilms.CustomComposables.Gradient
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun animatedGradient(
+    colors:List<Color>,
+    type:String = "horizontal",
+    animationSpec: AnimationSpec<Color> = tween(durationMillis = 220, delayMillis = 35)
+): Brush{
+
+    return when(type){
+        "horizontal" -> Brush.horizontalGradient(colors = colors.map { animateColorAsState(targetValue = it,animationSpec).value })
+        "vertical" -> Brush.verticalGradient(colors = colors.map { animateColorAsState(targetValue = it,animationSpec).value })
+        else -> Brush.horizontalGradient(colors = colors.map { animateColorAsState(targetValue = it,animationSpec).value })
+    }
+
+}
