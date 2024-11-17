8df357698fb1d08958353db56d59debae55d272a
ro-vezur <authenttesting@gmail.com>
2024-11-17 11:05:34 +0200

    feat: navigate to selected

--- app/src/main/java/com/example/jetfilms/Additional_functions/navigate/navigateToSelecedParticipant.kt
+++ app/src/main/java/com/example/jetfilms/Additional_functions/navigate/navigateToSelecedParticipant.kt
@@ -1,12 +1,12 @@
 package com.example.jetfilms.Additional_functions.navigate
 
 import androidx.navigation.NavController
-import com.example.jetfilms.Data_Classes.ParticipantPackage.DetailedMovieParticipantResponse
+import com.example.jetfilms.Data_Classes.ParticipantPackage.DetailedParticipantResponse
 import com.example.jetfilms.encodes.encodeStringWithSpecialCharacter
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
-fun navigateToSelectedParticipant(navController: NavController,selectedParticipantResponse: DetailedMovieParticipantResponse){
+fun navigateToSelectedParticipant(navController: NavController,selectedParticipantResponse: DetailedParticipantResponse){
     val jsonParticipant = Json.encodeToString(
         selectedParticipantResponse.copy(
             name = encodeStringWithSpecialCharacter(selectedParticipantResponse.name),
--- app/src/main/java/com/example/jetfilms/Additional_functions/navigate/navigateToSelectedMovie.kt
+++ app/src/main/java/com/example/jetfilms/Additional_functions/navigate/navigateToSelectedMovie.kt
@@ -1,12 +1,12 @@
 package com.example.jetfilms.Additional_functions.navigate
 
 import androidx.navigation.NavController
-import com.example.jetfilms.Data_Classes.MoviePackage.DetailedMovieDataClassResponse
+import com.example.jetfilms.Data_Classes.MoviePackage.DetailedMovieResponse
 import com.example.jetfilms.encodes.encodeStringWithSpecialCharacter
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
-fun navigateToSelectedMovie(navController: NavController, selectedMovie: DetailedMovieDataClassResponse){
+fun navigateToSelectedMovie(navController: NavController, selectedMovie: DetailedMovieResponse){
 
     val jsonMovie = Json.encodeToString(
         selectedMovie.copy(
