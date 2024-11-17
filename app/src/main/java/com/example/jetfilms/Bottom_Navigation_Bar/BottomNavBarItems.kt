e7c7562c1410711a438cdb6addb7af46975819f8
ro-vezur <authenttesting@gmail.com>
2024-11-17 10:50:57 +0200

    feat: bottom navigation bar

---
+++ app/src/main/java/com/example/jetfilms/Bottom_Navigation_Bar/BottomNavBarItems.kt
@@ -0,0 +1,17 @@
+package com.example.jetfilms.Bottom_Navigation_Bar
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LiveTv
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.BookmarkBorder
+import androidx.compose.ui.graphics.vector.ImageVector
+
+enum class BottomNavItems(val title: String,val icon:ImageVector,val route:Any){
+    HOME("Home",Icons.Filled.Home,"HomeScreen"),
+    EXPLORE("Explore",Icons.Filled.Search,"ExploreScreen"),
+    TV("TV",Icons.Filled.LiveTv,"TVScreen"),
+    FAVORITE("Favorite",Icons.Outlined.BookmarkBorder,"FavoriteScreen"),
+    ACCOUNT("Account",Icons.Filled.Person,"AccountScreen")
+}
