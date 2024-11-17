8a76c82354986ea55b92d3bd948fed39e20c318b
ro-vezur <authenttesting@gmail.com>
2024-11-17 11:07:41 +0200

    feat: select media genres screen

--- app/src/main/java/com/example/jetfilms/Screens/Start/Select_genres/SelectMediaGenresScreen.kt
+++ app/src/main/java/com/example/jetfilms/Screens/Start/Select_genres/SelectMediaGenresScreen.kt
@@ -1,12 +1,7 @@
 package com.example.jetfilms.Screens.Start.Select_genres
 
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.compose.animation.core.TwoWayConverter
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.animation.core.animateValueAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -19,13 +14,10 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
@@ -38,13 +30,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -52,26 +39,18 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.TextUnit
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import com.example.jetfilms.CustomComposables.TextButton
-import com.example.jetfilms.CustomComposables.TurnBackButton
-import com.example.jetfilms.R
+import com.example.jetfilms.CustomComposables.Buttons.TextButton
+import com.example.jetfilms.CustomComposables.Buttons.TurnBackButton
 import com.example.jetfilms.Screens.Start.SelectMediaFormatScreenRoute
-import com.example.jetfilms.Screens.Start.SignUpScreenRoute
 import com.example.jetfilms.baseButtonHeight
 import com.example.jetfilms.extensions.sdp
-import com.example.jetfilms.extensions.ssp
 import com.example.jetfilms.ui.theme.buttonsColor1
 import com.example.jetfilms.ui.theme.buttonsColor2
-import com.primex.core.VerticalGrid
-import com.primex.core.blur.legacyBackgroundBlur
-import com.primex.core.noise
 
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
