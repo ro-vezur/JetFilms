ac5b7e8c2bbf416394e7ca2654b197d08fce7680
ro-vezur <authenttesting@gmail.com>
2024-11-17 10:59:51 +0200

    feat: movies repository

---
+++ app/src/main/java/com/example/jetfilms/Repositories/MoviesRepository.kt
@@ -0,0 +1,34 @@
+package com.example.jetfilms.Repositories
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import com.example.jetfilms.API.ApiInterface
+import com.example.jetfilms.PAGESIZE
+import com.example.jetfilms.Pagination.MoviesPagingSource
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class MoviesRepository @Inject constructor(private val apiService: ApiInterface){
+    suspend fun getPopularMovies() = apiService.popularMovies(1)
+    fun getPaginatedPopularMovies(pagesLimit: Int = Int.MAX_VALUE) =
+        Pager(
+            config = PagingConfig(
+                pageSize = PAGESIZE,
+            ),
+            pagingSourceFactory = { MoviesPagingSource(apiService,"popular",pagesLimit) }
+        ).flow
+
+
+    suspend fun getTopRatedMovies() = apiService.topRatedMovies(1)
+
+    suspend fun getMovie(id: Int) = apiService.movie(id)
+
+    suspend fun getMovieCredits(movieId:Int) = apiService.movieCredits(movieId)
+
+    suspend fun getMovieImages(movieId: Int) = apiService.imagesFromMovie(movieId)
+
+    suspend fun getSimilarMovies(movieId: Int) = apiService.similarMovies(movieId)
+
+    suspend fun getParticipant(participantId: Int) = apiService.participant(participantId)
+}
