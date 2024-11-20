0e33d724e7f3423fbe0e3520d0470f3ab9bd9899
ro-vezur <authenttesting@gmail.com>
2024-11-21 00:23:12 +0200

    feat: movies view model

--- app/src/main/java/com/example/jetfilms/ViewModels/MoviesViewModel.kt
+++ app/src/main/java/com/example/jetfilms/ViewModels/MoviesViewModel.kt
@@ -2,12 +2,15 @@ package com.example.jetfilms.ViewModels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.jetfilms.Data_Classes.MoviePackage.DetailedMovieResponse
-import com.example.jetfilms.Data_Classes.ParticipantPackage.MovieCreditsResponse
-import com.example.jetfilms.Data_Classes.MoviePackage.MoviesResponse
-import com.example.jetfilms.Data_Classes.MoviePackage.SimplifiedMovieDataClass
-import com.example.jetfilms.Data_Classes.MoviePackage.ImagesFromTheMovieResponse
-import com.example.jetfilms.Data_Classes.ParticipantPackage.ParticipantFilmography
+import com.example.jetfilms.DTOs.MoviePackage.DetailedMovieResponse
+import com.example.jetfilms.DTOs.ParticipantPackage.MovieCreditsResponse
+import com.example.jetfilms.DTOs.MoviePackage.MoviesResponse
+import com.example.jetfilms.DTOs.MoviePackage.SimplifiedMovieDataClass
+import com.example.jetfilms.DTOs.MoviePackage.ImagesFromTheMovieResponse
+import com.example.jetfilms.DTOs.ParticipantPackage.ParticipantFilmography
+import com.example.jetfilms.DTOs.ParticipantPackage.ParticipantImagesResponse
+import com.example.jetfilms.DTOs.SeriesPackage.DetailedSerialResponse
+import com.example.jetfilms.DTOs.SeriesPackage.SimplifiedSerialsResponse
 import com.example.jetfilms.Repositories.MoviesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -30,8 +33,12 @@ class MoviesViewModel @Inject constructor(
     private val _similarMovies = MutableStateFlow<MoviesResponse?>(null)
     val similarMovies = _similarMovies.asStateFlow()
 
-    private val _moreMoviesView:MutableStateFlow<List<SimplifiedMovieDataClass>> = MutableStateFlow(emptyList())
+    private val _moreMoviesView: MutableStateFlow<List<SimplifiedMovieDataClass>> = MutableStateFlow(emptyList())
     val moreMoviesView = _moreMoviesView.asStateFlow()
+
+    private val _moreSerialsView: MutableStateFlow<SimplifiedSerialsResponse?> = MutableStateFlow(null)
+    val moreSerialsView = _moreSerialsView.asStateFlow()
+
 
     private val _selectedMovie = MutableStateFlow<DetailedMovieResponse?>(null)
     val selectedMovie = _selectedMovie.asStateFlow()
@@ -42,19 +49,29 @@ class MoviesViewModel @Inject constructor(
     private val _selectedMovieImages = MutableStateFlow(ImagesFromTheMovieResponse())
     val selectedMovieImages = _selectedMovieImages.asStateFlow()
 
+
+    private val _popularSerials = MutableStateFlow<SimplifiedSerialsResponse?>(null)
+    val  popularSerials = _popularSerials.asStateFlow()
+
+
     private val _selectedParticipantFilmography = MutableStateFlow<ParticipantFilmography?>(null)
     val selectedParticipantFilmography = _selectedParticipantFilmography.asStateFlow()
+
+    private val _selectedParticipantImages = MutableStateFlow<ParticipantImagesResponse?>(null)
+    val selectedParticipantImages = _selectedParticipantImages.asStateFlow()
     init {
         viewModelScope.launch {
             withContext(Dispatchers.IO){
-                    _topRatedMovies.emit(moviesRepository.getTopRatedMovies().results)
-                    if (_topRatedMovies.value.isNotEmpty()) {
-                        setSelectedMovie(_topRatedMovies.value[0].id)
-                    }
-                    getPopularMovies()
+                _topRatedMovies.emit(moviesRepository.getTopRatedMovies().results)
+                if (_topRatedMovies.value.isNotEmpty()) {
+                    setSelectedMovie(_topRatedMovies.value[0].id)
+                }
+                getPopularMovies()
+                setPopularSerials()
             }
         }
     }
+
 
     fun setSelectedMovie(movieId:Int){
         viewModelScope.launch {
@@ -74,6 +91,7 @@ class MoviesViewModel @Inject constructor(
         }
     }
 
+
     fun getPopularMovies(){
         viewModelScope.launch {
             withContext(Dispatchers.IO){
@@ -90,6 +108,23 @@ class MoviesViewModel @Inject constructor(
         }
     }
 
+    fun setMoreSerialsView(serials:SimplifiedSerialsResponse){
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                _moreSerialsView.emit(serials)
+            }
+        }
+    }
+
+    fun setPopularSerials(){
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                _popularSerials.emit(moviesRepository.getPopularSerials())
+            }
+        }
+    }
+
+
     fun setParticipantFilmography(participantId: Int){
         viewModelScope.launch {
             withContext(Dispatchers.IO){
@@ -98,8 +133,19 @@ class MoviesViewModel @Inject constructor(
         }
     }
 
-
+    fun setParticipantImages(participantId: Int){
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                _selectedParticipantImages.emit(moviesRepository.getParticipantImages(participantId))
+            }
+        }
+    }
 
     suspend fun getMovie(movieId: Int): DetailedMovieResponse? = moviesRepository.getMovie(movieId).body()
+
+    suspend fun getSerial(serialId: Int): DetailedSerialResponse = moviesRepository.getSerial(serialId)
+
+    suspend fun getSerialSeason(serialId: Int,seasonNumber: Int) = moviesRepository.getSerialSeason(serialId,seasonNumber)
+
     suspend fun getParticipant(participantId: Int) = moviesRepository.getParticipant(participantId)
 }
