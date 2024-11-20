17b5487a4d266d7e1bbc3ce2de01c9728f48a24b
ro-vezur <authenttesting@gmail.com>
2024-11-21 00:26:28 +0200

    feat: Constants

--- app/src/main/java/com/example/jetfilms/Constants.kt
+++ app/src/main/java/com/example/jetfilms/Constants.kt
@@ -1,23 +1,20 @@
 package com.example.jetfilms
 
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import com.example.jetfilms.ui.theme.buttonsColor1
 import com.example.jetfilms.ui.theme.buttonsColor2
 
-const val baseButtonWidth = 250
-const val baseButtonHeight = 40
+const val BASE_BUTTON_WIDTH = 250
+const val BASE_BUTTON_HEIGHT = 40
 
-const val bottomNavBarHeight = 54
-const val bottomNavItemSize = 28
+const val BOTTOM_NAVIGATION_BAR_HEIGHT = 50
+const val BOTTOM_NAVIGATION_ITEM_SIZE = 28
 
 val blueHorizontalGradient = Brush.horizontalGradient(listOf(buttonsColor1, buttonsColor2))
 val blueVerticalGradient = Brush.horizontalGradient(listOf(buttonsColor1, buttonsColor2))
 
-const val PAGESIZE = 20
+const val PAGE_SIZE = 20
 
-const val baseAPIUrl = "https://api.themoviedb.org/3/"
-const val baseImageUrl = "https://image.tmdb.org/t/p/w500"
+const val BASE_API_URL = "https://api.themoviedb.org/3/"
+const val BASE_IMAGE_API_URL = "https://image.tmdb.org/t/p/w500"
 const val APIKEY = ""
-
-val minutesInHour = 60
