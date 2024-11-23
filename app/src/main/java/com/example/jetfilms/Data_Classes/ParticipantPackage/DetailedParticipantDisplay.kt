f412251876ef4c6c013d11b910a75495abd45eb0
ro-vezur <authenttesting@gmail.com>
2024-11-24 00:34:39 +0200

    feat: participant DTOs

--- app/src/main/java/com/example/jetfilms/Data_Classes/ParticipantPackage/DetailedParticipantDisplay.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jetfilms.Data_Classes.ParticipantPackage
-
-data class DetailedParticipantDisplay(
-    val participantResponse: DetailedParticipantResponse,
-    val filmography: ParticipantFilmography
-)
