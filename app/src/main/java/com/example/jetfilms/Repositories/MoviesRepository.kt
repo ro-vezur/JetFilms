15c3f5c693a23f51a53862b77ccbeb93486cffc6
ro-vezur <authenttesting@gmail.com>
2024-11-24 00:31:50 +0200

    feat: movies repository

--- app/src/main/java/com/example/jetfilms/Repositories/MoviesRepository.kt
+++ app/src/main/java/com/example/jetfilms/Repositories/MoviesRepository.kt
@@ -3,22 +3,33 @@ package com.example.jetfilms.Repositories
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import com.example.jetfilms.API.ApiInterface
+import com.example.jetfilms.DTOs.MoviePackage.MoviesResponse
+import com.example.jetfilms.DTOs.SeriesPackage.SimplifiedSerialsResponse
 import com.example.jetfilms.PAGE_SIZE
 import com.example.jetfilms.Helpers.Pagination.MoviesPagingSource
+import com.example.jetfilms.Helpers.Pagination.SerialsPagingSource
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class MoviesRepository @Inject constructor(private val apiService: ApiInterface){
-    suspend fun getPopularMovies() = apiService.popularMovies(1)
-    fun getPaginatedPopularMovies(pagesLimit: Int = Int.MAX_VALUE) =
+    suspend fun getPopularMovies(page: Int) = apiService.popularMovies(page)
+    fun getPaginatedMovies(getResponse: suspend (page: Int) -> MoviesResponse, pagesLimit: Int = Int.MAX_VALUE) =
         Pager(
             config = PagingConfig(
                 pageSize = PAGE_SIZE,
             ),
-            pagingSourceFactory = { MoviesPagingSource(getResponse = {page -> apiService.popularMovies(page) },pagesLimit) }
+            pagingSourceFactory = { MoviesPagingSource(getResponse = {page -> getResponse(page) },pagesLimit) }
         ).flow
 
+    fun getPaginatedSerials(getResponse: suspend (page: Int) -> SimplifiedSerialsResponse, pagesLimit: Int = Int.MAX_VALUE) =
+        Pager(
+            config = PagingConfig(
+                pageSize = PAGE_SIZE,
+            ),
+            pagingSourceFactory = { SerialsPagingSource(getResponse = {page -> getResponse(page) },pagesLimit) }
+        ).flow
 
     suspend fun getTopRatedMovies() = apiService.topRatedMovies(1)
 
@@ -30,7 +41,7 @@ class MoviesRepository @Inject constructor(private val apiService: ApiInterface)
 
     suspend fun getSimilarMovies(movieId: Int) = apiService.similarMovies(movieId)
 
-    suspend fun getPopularSerials() = apiService.popularSerials(1)
+    suspend fun getPopularSerials(page: Int) = apiService.popularSerials(page)
 
     suspend fun getSerial(serialId: Int) = apiService.serial(serialId)
 
@@ -41,4 +52,8 @@ class MoviesRepository @Inject constructor(private val apiService: ApiInterface)
     suspend fun getParticipantFilmography(participantId: Int) = apiService.participantFilmography(participantId)
 
     suspend fun getParticipantImages(participantId: Int) = apiService.participantImages(participantId)
+
+    suspend fun searchMovies(query: String, page: Int) = apiService.searchMovies(query,page)
+
+    suspend fun searchSerials(query: String,page: Int) = apiService.searchSerials(query,page)
 }
