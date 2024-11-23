69cb6b429d1548034bc746f56889d53c9e45e7d8
ro-vezur <authenttesting@gmail.com>
2024-11-24 00:31:10 +0200

    feat: movies view model

--- app/src/main/java/com/example/jetfilms/ViewModels/MoviesViewModel.kt
+++ app/src/main/java/com/example/jetfilms/ViewModels/MoviesViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.jetfilms.ViewModels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
 import com.example.jetfilms.DTOs.MoviePackage.DetailedMovieResponse
 import com.example.jetfilms.DTOs.ParticipantPackage.MovieCreditsResponse
 import com.example.jetfilms.DTOs.MoviePackage.MoviesResponse
@@ -9,13 +13,19 @@ import com.example.jetfilms.DTOs.MoviePackage.SimplifiedMovieDataClass
 import com.example.jetfilms.DTOs.MoviePackage.ImagesFromTheMovieResponse
 import com.example.jetfilms.DTOs.ParticipantPackage.ParticipantFilmography
 import com.example.jetfilms.DTOs.ParticipantPackage.ParticipantImagesResponse
+import com.example.jetfilms.DTOs.SearchObject
 import com.example.jetfilms.DTOs.SeriesPackage.DetailedSerialResponse
+import com.example.jetfilms.DTOs.SeriesPackage.SimplifiedSerialObject
 import com.example.jetfilms.DTOs.SeriesPackage.SimplifiedSerialsResponse
 import com.example.jetfilms.Repositories.MoviesRepository
+import com.example.jetfilms.Screens.Start.Select_type.MediaFormats
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -33,12 +43,11 @@ class MoviesViewModel @Inject constructor(
     private val _similarMovies = MutableStateFlow<MoviesResponse?>(null)
     val similarMovies = _similarMovies.asStateFlow()
 
-    private val _moreMoviesView: MutableStateFlow<List<SimplifiedMovieDataClass>> = MutableStateFlow(emptyList())
+    private val _moreMoviesView: MutableStateFlow<PagingData<SimplifiedMovieDataClass>> = MutableStateFlow(PagingData.empty())
     val moreMoviesView = _moreMoviesView.asStateFlow()
 
-    private val _moreSerialsView: MutableStateFlow<SimplifiedSerialsResponse?> = MutableStateFlow(null)
+    private val _moreSerialsView: MutableStateFlow<PagingData<SimplifiedSerialObject>> = MutableStateFlow(PagingData.empty())
     val moreSerialsView = _moreSerialsView.asStateFlow()
-
 
     private val _selectedMovie = MutableStateFlow<DetailedMovieResponse?>(null)
     val selectedMovie = _selectedMovie.asStateFlow()
@@ -50,7 +59,7 @@ class MoviesViewModel @Inject constructor(
     val selectedMovieImages = _selectedMovieImages.asStateFlow()
 
 
-    private val _popularSerials = MutableStateFlow<SimplifiedSerialsResponse?>(null)
+    private val _popularSerials = MutableStateFlow<List<SimplifiedSerialObject>>(listOf())
     val  popularSerials = _popularSerials.asStateFlow()
 
 
@@ -59,6 +68,22 @@ class MoviesViewModel @Inject constructor(
 
     private val _selectedParticipantImages = MutableStateFlow<ParticipantImagesResponse?>(null)
     val selectedParticipantImages = _selectedParticipantImages.asStateFlow()
+
+    private val _searchedData = MutableStateFlow<List<SearchObject>>(listOf())
+    val searchedData = _searchedData.asStateFlow()
+
+    private val _searchedMovies = MutableStateFlow<MoviesResponse?>(null)
+    val searchedMovies = _searchedMovies.asStateFlow()
+
+    private val _searchedSerial = MutableStateFlow<SimplifiedSerialsResponse?>(null)
+    val searchedSerials = _searchedSerial.asStateFlow()
+
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    private val _requestSent = MutableStateFlow(false)
+    val requestSent = _requestSent.asStateFlow()
+
     init {
         viewModelScope.launch {
             withContext(Dispatchers.IO){
@@ -66,7 +91,7 @@ class MoviesViewModel @Inject constructor(
                 if (_topRatedMovies.value.isNotEmpty()) {
                     setSelectedMovie(_topRatedMovies.value[0].id)
                 }
-                getPopularMovies()
+                setPopularMovies()
                 setPopularSerials()
             }
         }
@@ -92,26 +117,38 @@ class MoviesViewModel @Inject constructor(
     }
 
 
-    fun getPopularMovies(){
+    fun setPopularMovies(){
         viewModelScope.launch {
             withContext(Dispatchers.IO){
-                _popularMovies.emit(moviesRepository.getPopularMovies().results)
+                _popularMovies.emit(moviesRepository.getPopularMovies(1).results)
             }
         }
     }
 
-    fun setMoreMoviesView(movies:List<SimplifiedMovieDataClass>){
+    fun setMoreMoviesView(response: suspend (page: Int) -> MoviesResponse,pageLimit: Int = Int.MAX_VALUE){
         viewModelScope.launch {
             withContext(Dispatchers.IO){
-                _moreMoviesView.emit(movies)
-            }
+                val paginatedMovies = moviesRepository.getPaginatedMovies(response,pageLimit)
+
+                paginatedMovies
+                    .cachedIn(viewModelScope)
+                    .collect {
+                        _moreMoviesView.emit(it)
+                    }
+                }
         }
     }
 
-    fun setMoreSerialsView(serials:SimplifiedSerialsResponse){
+    fun setMoreSerialsView(response: suspend (page: Int) -> SimplifiedSerialsResponse,pageLimit: Int = Int.MAX_VALUE){
         viewModelScope.launch {
             withContext(Dispatchers.IO){
-                _moreSerialsView.emit(serials)
+                val paginatedSerials = moviesRepository.getPaginatedSerials(response,pageLimit)
+
+                paginatedSerials
+                    .cachedIn(viewModelScope)
+                    .collect{
+                        _moreSerialsView.emit(it)
+                    }
             }
         }
     }
@@ -119,7 +156,7 @@ class MoviesViewModel @Inject constructor(
     fun setPopularSerials(){
         viewModelScope.launch {
             withContext(Dispatchers.IO){
-                _popularSerials.emit(moviesRepository.getPopularSerials())
+                _popularSerials.emit(moviesRepository.getPopularSerials(1).results)
             }
         }
     }
@@ -141,6 +178,44 @@ class MoviesViewModel @Inject constructor(
         }
     }
 
+    fun setSearchedMovies(query: String?){
+        viewModelScope.launch{
+            withContext(Dispatchers.IO){
+                    _searchedMovies.emit(
+                        if(query.isNullOrBlank()) null
+                        else moviesRepository.searchMovies(query.toString(), 1)
+                    )
+            }
+        }
+    }
+
+    fun setSearchedSerials(query: String?){
+        viewModelScope.launch{
+            withContext(Dispatchers.IO){
+                _searchedSerial.emit(
+                   if(query.isNullOrBlank()) null
+                   else moviesRepository.searchSerials(query,1)
+                )
+            }
+        }
+    }
+
+    fun setSearchText(text: String){
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                _searchText.emit(text)
+            }
+        }
+    }
+
+    fun setIsRequestSent(sent: Boolean){
+        viewModelScope.launch{
+            withContext(Dispatchers.IO){
+                _requestSent.emit(sent)
+            }
+        }
+    }
+
     suspend fun getMovie(movieId: Int): DetailedMovieResponse? = moviesRepository.getMovie(movieId).body()
 
     suspend fun getSerial(serialId: Int): DetailedSerialResponse = moviesRepository.getSerial(serialId)
@@ -148,4 +223,12 @@ class MoviesViewModel @Inject constructor(
     suspend fun getSerialSeason(serialId: Int,seasonNumber: Int) = moviesRepository.getSerialSeason(serialId,seasonNumber)
 
     suspend fun getParticipant(participantId: Int) = moviesRepository.getParticipant(participantId)
+
+    suspend fun searchMovies(query: String, page: Int) = moviesRepository.searchMovies(query,page)
+
+    suspend fun searchSerials(query: String,page: Int) = moviesRepository.searchSerials(query,page)
+
+    suspend fun getPopularMovies(page: Int) = moviesRepository.getPopularMovies(page)
+
+    suspend fun getPopularSerials(page: Int) = moviesRepository.getPopularSerials(page)
 }
