8d935288538c4e4f4458f80c9fe4ace7b329763d
ro-vezur <authenttesting@gmail.com>
2024-11-28 01:19:14 +0200

    feat: gradient Components

--- app/src/main/java/com/example/jetfilms/Components/Gradient/GradientWithAnimation.kt
+++ app/src/main/java/com/example/jetfilms/Components/Gradient/GradientWithAnimation.kt
@@ -6,18 +6,18 @@ import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import com.example.jetfilms.DTOs.animatedGradientTypes
 
 @Composable
 fun animatedGradient(
     colors:List<Color>,
-    type:String = "horizontal",
+    type: animatedGradientTypes = animatedGradientTypes.HORIZONTAL,
     animationSpec: AnimationSpec<Color> = tween(durationMillis = 220, delayMillis = 35)
 ): Brush{
 
     return when(type){
-        "horizontal" -> Brush.horizontalGradient(colors = colors.map { animateColorAsState(targetValue = it,animationSpec).value })
-        "vertical" -> Brush.verticalGradient(colors = colors.map { animateColorAsState(targetValue = it,animationSpec).value })
-        else -> Brush.horizontalGradient(colors = colors.map { animateColorAsState(targetValue = it,animationSpec).value })
+        animatedGradientTypes.HORIZONTAL -> Brush.horizontalGradient(colors = colors.map { animateColorAsState(targetValue = it,animationSpec).value })
+        animatedGradientTypes.VERTICAL -> Brush.verticalGradient(colors = colors.map { animateColorAsState(targetValue = it,animationSpec).value })
     }
 
 }
