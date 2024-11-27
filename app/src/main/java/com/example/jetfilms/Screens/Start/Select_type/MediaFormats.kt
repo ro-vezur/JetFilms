80354f481dbd73a16fc1d39c11cfee2d923b4b6e
ro-vezur <authenttesting@gmail.com>
2024-11-28 01:22:57 +0200

    feat: Media formats

--- app/src/main/java/com/example/jetfilms/Screens/Start/Select_type/MediaFormats.kt
+++ app/src/main/java/com/example/jetfilms/Screens/Start/Select_type/MediaFormats.kt
@@ -1,7 +1,6 @@
 package com.example.jetfilms.Screens.Start.Select_type
 
 enum class MediaFormats(val format:String,val imageUrl:String) {
-    MOVIE("Movie","https://images.squarespace-cdn.com/content/v1/63bb3e8a824d7e2f7eedf0d3/1689034557973-HC4X5JFW3GJVWA8XPDUF/Oppenheimer+Horizontal.jpeg"),
+    MOVIE("Movies","https://images.squarespace-cdn.com/content/v1/63bb3e8a824d7e2f7eedf0d3/1689034557973-HC4X5JFW3GJVWA8XPDUF/Oppenheimer+Horizontal.jpeg"),
     SERIES("Series","https://savethecat.com/wp-content/uploads/2016/06/Game-of-Thrones-Season-1.jpg"),
-    CARTOONS("Cartoons","https://i.redd.it/73iglnwr4qxc1.jpeg");
 }
