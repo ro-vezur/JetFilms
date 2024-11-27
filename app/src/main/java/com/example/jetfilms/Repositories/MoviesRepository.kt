fae669224ade28f2049f66d2a780322d39f06e7b
ro-vezur <authenttesting@gmail.com>
2024-11-28 01:20:25 +0200

    feat: movies repository

--- app/src/main/java/com/example/jetfilms/Repositories/MoviesRepository.kt
+++ app/src/main/java/com/example/jetfilms/Repositories/MoviesRepository.kt
@@ -3,12 +3,18 @@ package com.example.jetfilms.Repositories
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import com.example.jetfilms.API.ApiInterface
+import com.example.jetfilms.BASE_MEDIA_GENRES
 import com.example.jetfilms.DTOs.MoviePackage.MoviesResponse
 import com.example.jetfilms.DTOs.SeriesPackage.SimplifiedSerialsResponse
+import com.example.jetfilms.DTOs.Filters.SortTypes
+import com.example.jetfilms.Helpers.ListToString.CountryListToString
+import com.example.jetfilms.Helpers.ListToString.IntListToString
+import com.example.jetfilms.Helpers.ListToString.StringListToString
 import com.example.jetfilms.PAGE_SIZE
 import com.example.jetfilms.Helpers.Pagination.MoviesPagingSource
 import com.example.jetfilms.Helpers.Pagination.SerialsPagingSource
-import kotlinx.coroutines.flow.map
+import com.example.jetfilms.Helpers.Pagination.UnifiedPagingSource
+import com.example.jetfilms.Screens.Start.Select_type.MediaFormats
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -30,6 +36,25 @@ class MoviesRepository @Inject constructor(private val apiService: ApiInterface)
             ),
             pagingSourceFactory = { SerialsPagingSource(getResponse = {page -> getResponse(page) },pagesLimit) }
         ).flow
+
+    fun getPaginatedUnifiedData(
+        getMoviesResponse: suspend (page: Int) -> MoviesResponse,
+        getSerialsResponse: suspend (page: Int) -> SimplifiedSerialsResponse,
+        sortType: SortTypes?,
+        categories: List<MediaFormats>,
+        pagesLimit: Int = Int.MAX_VALUE
+    ) = Pager(
+        config = PagingConfig(
+            pageSize = PAGE_SIZE,
+        ),
+        pagingSourceFactory = {UnifiedPagingSource(
+            getMoviesResponse = getMoviesResponse,
+            getSerialsResponse = getSerialsResponse,
+            sortType = sortType,
+            categories = categories,
+            pagesLimit = pagesLimit
+        )}
+    ).flow
 
     suspend fun getTopRatedMovies() = apiService.topRatedMovies(1)
 
@@ -53,7 +78,43 @@ class MoviesRepository @Inject constructor(private val apiService: ApiInterface)
 
     suspend fun getParticipantImages(participantId: Int) = apiService.participantImages(participantId)
 
-    suspend fun searchMovies(query: String, page: Int) = apiService.searchMovies(query,page)
+    suspend fun searchMovies(query: String,page: Int) = apiService.searchMovies(query,page)
 
     suspend fun searchSerials(query: String,page: Int) = apiService.searchSerials(query,page)
+
+    suspend fun discoverMovies(page: Int,sortBy: String,genres: List<Int>,countries:List<String>): MoviesResponse {
+        return if(genres == BASE_MEDIA_GENRES.map { it.genreId }) {
+            apiService.discoverMovies(
+                page = page,
+                sortBy = sortBy,
+                countries = CountryListToString(countries),
+            )
+        }
+        else {
+            apiService.filteredGenresDiscoverMovies(
+                page = page,
+                sortBy = sortBy,
+                genres = IntListToString(genres),
+                countries = CountryListToString(countries),
+            )
+        }
+    }
+
+    suspend fun discoverSerials(page: Int,sortBy: String,genres: List<Int>,countries: List<String>): SimplifiedSerialsResponse {
+        return if(genres == BASE_MEDIA_GENRES.map { it.genreId }) {
+            apiService.discoverSerials(
+                page = page,
+                sortBy = sortBy,
+                countries = CountryListToString(countries),
+            )
+        }
+        else {
+            apiService.filteredGenresDiscoverSerials(
+                page = page,
+                sortBy = sortBy,
+                genres = IntListToString(genres),
+                countries = CountryListToString(countries),
+            )
+        }
+    }
 }
