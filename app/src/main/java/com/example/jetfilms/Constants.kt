241664b792e86c32b5b74ce3ad2821e697c41470
ro-vezur <authenttesting@gmail.com>
2024-11-29 01:59:43 +0200

    feat: constants

--- app/src/main/java/com/example/jetfilms/Constants.kt
+++ app/src/main/java/com/example/jetfilms/Constants.kt
@@ -14,15 +14,24 @@ const val BOTTOM_NAVIGATION_ITEM_SIZE = 28
 
 const val FILTER_TOP_BAR_HEIGHT = 45
 
-val blueHorizontalGradient = Brush.horizontalGradient(listOf(buttonsColor1, buttonsColor2))
-val blueVerticalGradient = Brush.horizontalGradient(listOf(buttonsColor1, buttonsColor2))
-val whiteGradient = Brush.horizontalGradient(listOf(whiteColor, whiteColor))
+val blueGradientColors = listOf(buttonsColor1, buttonsColor2)
+val whiteGradientColors = listOf(whiteColor, whiteColor)
+
+val blueHorizontalGradient = Brush.horizontalGradient(blueGradientColors)
+val blueVerticalGradient = Brush.horizontalGradient(blueGradientColors)
+val whiteGradient = Brush.horizontalGradient(whiteGradientColors)
 
 const val PAGE_SIZE = 20
 
 const val HAZE_STATE_BLUR = 28
 
 val BASE_MEDIA_GENRES = MediaGenres.entries.drop(1).sorted()
+
+val infoTabs = listOf(
+    "Trailers",
+    "More Like This",
+    "About"
+)
 
 const val BASE_API_URL = "https://api.themoviedb.org/3/"
 const val BASE_IMAGE_API_URL = "https://image.tmdb.org/t/p/w500"
