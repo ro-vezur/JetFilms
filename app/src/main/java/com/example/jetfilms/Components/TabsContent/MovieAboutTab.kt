61ad310b237fe025d03b1c03de2c46aca76f2deb
ro-vezur <authenttesting@gmail.com>
2024-11-29 01:53:51 +0200

    feat: tabs content pager

---
+++ app/src/main/java/com/example/jetfilms/Components/TabsContent/MovieAboutTab.kt
@@ -0,0 +1,49 @@
+package com.example.jetfilms.Components.TabsContent
+
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.jetfilms.DTOs.MoviePackage.MovieDisplay
+import com.example.jetfilms.DTOs.MoviePackage.SimplifiedMovieDataClass
+import com.example.jetfilms.DTOs.UnifiedDataPackage.SimplifiedParticipantResponse
+import com.example.jetfilms.Screens.MovieDetailsPackage.MovieAboutScreen
+import com.example.jetfilms.Screens.MovieDetailsPackage.MovieMoreLikeThisScreen
+import com.example.jetfilms.Screens.MovieDetailsPackage.MovieTrailersScreen
+
+@Composable
+fun MovieAboutTab(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState,
+    movieDisplay: MovieDisplay,
+    selectMovie: (movie: SimplifiedMovieDataClass) -> Unit,
+    navigateToSelectedParticipant: (participant: SimplifiedParticipantResponse) -> Unit
+) {
+    val movieResponse = movieDisplay.response
+
+    HorizontalPager(
+        state = pagerState,
+        modifier = modifier,
+        userScrollEnabled = false
+    ) { index ->
+        when (index) {
+            0 -> {
+                MovieTrailersScreen(movie = movieResponse)
+            }
+
+            1 -> {
+                MovieMoreLikeThisScreen(
+                    similarMovies = movieDisplay.similarMovies,
+                    selectMovie = selectMovie
+                )
+            }
+
+            2 -> {
+                MovieAboutScreen(
+                    movieDisplay = movieDisplay,
+                    navigateToSelectedParticipant = navigateToSelectedParticipant
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/jetfilms/Components/TabsContent/SeriesAboutTab.kt
@@ -0,0 +1,48 @@
+package com.example.jetfilms.Components.TabsContent
+
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.jetfilms.DTOs.SeriesPackage.SerialDisplay
+import com.example.jetfilms.DTOs.SeriesPackage.SimplifiedSerialObject
+import com.example.jetfilms.DTOs.UnifiedDataPackage.SimplifiedParticipantResponse
+import com.example.jetfilms.Screens.SerialDetailsPackage.SerialAboutScreen
+import com.example.jetfilms.Screens.SerialDetailsPackage.SerialMoreLikeThisScreen
+
+@Composable
+fun SeriesAboutTab(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState,
+    seriesDisplay: SerialDisplay,
+    selectSeries: (movie: SimplifiedSerialObject) -> Unit,
+    navigateToSelectedParticipant: (participant: SimplifiedParticipantResponse) -> Unit
+) {
+    val seriesResponse = seriesDisplay.response
+
+    HorizontalPager(
+        state = pagerState,
+        modifier = modifier,
+        userScrollEnabled = false
+    ) { index ->
+        when (index) {
+            0 -> {
+
+            }
+
+            1 -> {
+                SerialMoreLikeThisScreen(
+                    similarSeries = seriesDisplay.similarSerials,
+                    selectSerial = selectSeries,
+                )
+            }
+
+            2 -> {
+                SerialAboutScreen(
+                    seriesDisplay = seriesDisplay,
+                    navigateToSelectedParticipant = navigateToSelectedParticipant,
+                )
+            }
+        }
+    }
+}
