a3cfd02a43004cc09d73fefb8dfed573c4324f3a
ro-vezur <authenttesting@gmail.com>
2024-12-03 00:59:43 +0200

    feat: cards

--- app/src/main/java/com/example/jetfilms/Components/Cards/UnifiedCard.kt
+++ app/src/main/java/com/example/jetfilms/Components/Cards/UnifiedCard.kt
@@ -15,8 +15,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import coil.compose.AsyncImage
+import coil.request.CachePolicy
+import coil.request.ImageRequest
 import com.example.jetfilms.BASE_IMAGE_API_URL
 import com.example.jetfilms.DTOs.UnifiedDataPackage.UnifiedMedia
 import com.example.jetfilms.Helpers.removeNumbersAfterDecimal
@@ -29,6 +32,8 @@ fun UnifiedCard(
     modifier: Modifier = Modifier,
     unifiedMedia: UnifiedMedia,
 ) {
+    val context = LocalContext.current
+
     Box(
         modifier = modifier
     ){
