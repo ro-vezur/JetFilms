96710c1313f6d89c33ed77ecef24e76b1f255488
ro-vezur <authenttesting@gmail.com>
2024-12-03 01:01:24 +0200

    feat: date functions

--- app/src/main/java/com/example/jetfilms/Helpers/Date_formats/Date Formats.kt
+++ app/src/main/java/com/example/jetfilms/Helpers/Date_formats/Date Formats.kt
@@ -1,13 +1,15 @@
 package com.example.jetfilms.Helpers.Date_formats
 
+import android.icu.text.SimpleDateFormat
 import android.util.Log
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.Date
 
 class DateFormats {
     var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
-    fun year(dateString: String): Int {
+    fun getYear(dateString: String): Int {
         return  try {
             val date = LocalDate.parse(dateString, formatter)
              date.year
@@ -15,6 +17,12 @@ class DateFormats {
         catch (e:Exception){
             Log.e("error",e.message.toString())
             0
+        }
+    }
+    companion object{
+        fun getCurrentDate(): String {
+            val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+            return sdf.format(Date())
         }
     }
 }
