b7ee8f56da491549b6238f61f0ac5f9181c649f6
ro-vezur <authenttesting@gmail.com>
2024-12-07 23:18:29 +0200

    feat: Constants

--- app/src/main/java/com/example/jetfilms/Constants.kt
+++ app/src/main/java/com/example/jetfilms/Constants.kt
@@ -35,7 +35,9 @@ val infoTabs = listOf(
 
 const val BASE_API_URL = "https://api.themoviedb.org/3/"
 const val BASE_IMAGE_API_URL = "https://image.tmdb.org/t/p/w500"
-const val API_KEY = "d15e6f4b2afbe36b05158477b6aaedba"
+const val API_KEY = ""
 
 const val BASE_YOUTUBE_IMAGES_URL = "https://img.youtube.com/vi/"
 const val YOUTUBE_IMAGE_THUMBNAIL_TYPE = "/mqdefault.jpg"
+
+const val USERS_COLLECTION = "users"
