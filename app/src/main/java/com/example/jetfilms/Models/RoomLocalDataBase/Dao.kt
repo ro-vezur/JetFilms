3342582a18867de91585a56fbba2c4f1c66ceeb0
ro-vezur <authenttesting@gmail.com>
2024-12-15 23:59:49 +0200

    feat: room DAO

--- app/src/main/java/com/example/jetfilms/Models/RoomLocalDataBase/Dao.kt
+++ app/src/main/java/com/example/jetfilms/Models/RoomLocalDataBase/Dao.kt
@@ -2,18 +2,19 @@ package com.example.jetfilms.Models.RoomLocalDataBase
 
 import androidx.room.Dao
 import androidx.room.Delete
-import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Upsert
 import com.example.jetfilms.Models.DTOs.SearchHistory_RoomDb.SearchedMedia
-import kotlinx.coroutines.flow.Flow
 
 
 @Dao
-interface DataBaseDao {
+interface Dao {
 
-    @Query("Select * FROM searchHistory")
+    @Query("Select * From searchHistory")
     suspend fun getAllSearches(): List<SearchedMedia>
+
+    @Query("Select * From searchHistory Where id == :id LIMIT 1")
+    suspend fun getSearch(id: String): SearchedMedia
 
     @Upsert
     suspend fun insertSearchedMedia(searchedMedia: SearchedMedia)
