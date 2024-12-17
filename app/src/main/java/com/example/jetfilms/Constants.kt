35a8b702acaf0d93f3c560c14df2e6b4e6078832
ro-vezur <authenttesting@gmail.com>
2024-12-17 17:37:29 +0200

    feat: constants

--- app/src/main/java/com/example/jetfilms/Constants.kt
+++ app/src/main/java/com/example/jetfilms/Constants.kt
@@ -2,6 +2,7 @@ package com.example.jetfilms
 
 import androidx.compose.ui.graphics.Brush
 import com.example.jetfilms.View.Screens.Start.Select_genres.MediaGenres
+import com.example.jetfilms.View.Screens.Start.Select_type.MediaCategories
 import com.example.jetfilms.ui.theme.buttonsColor1
 import com.example.jetfilms.ui.theme.buttonsColor2
 import com.example.jetfilms.ui.theme.whiteColor
@@ -13,6 +14,8 @@ const val BOTTOM_NAVIGATION_BAR_HEIGHT = 50
 const val BOTTOM_NAVIGATION_ITEM_SIZE = 26
 
 const val FILTER_TOP_BAR_HEIGHT = 45
+
+const val TEXT_FIELD_MAX_LENGTH = 300
 
 val blueGradientColors = listOf(buttonsColor1, buttonsColor2)
 val whiteGradientColors = listOf(whiteColor, whiteColor)
@@ -26,6 +29,7 @@ const val PAGE_SIZE = 20
 const val HAZE_STATE_BLUR = 28
 
 val BASE_MEDIA_GENRES = MediaGenres.entries.drop(1).sorted()
+val BASE_MEDIA_CATEGORIES = MediaCategories.entries.toList()
 
 val infoTabs = listOf(
     "Trailers",
@@ -41,3 +45,17 @@ const val BASE_YOUTUBE_IMAGES_URL = "https://img.youtube.com/vi/"
 const val YOUTUBE_IMAGE_THUMBNAIL_TYPE = "/mqdefault.jpg"
 
 const val USERS_COLLECTION = "users"
+
+const val APP_DESCRIPTION = "Welcome to JetFilms, your ultimate app for discovering movies and TV shows!\n" +
+        "\n" +
+        "\uD83C\uDF1F Key Features:\n" +
+        "\n" +
+        "Explore a vast library of movies and TV series.\n" +
+        "Save your favorites for later.\n" +
+        "Watch trailers and learn about cast members.\n" +
+        "Filter by genres, ratings, or release years.\n" +
+        "Discover similar recommendations effortlessly.\n" +
+        "\n" +
+        "\uD83D\uDEE0 Version 1.0.0\n" +
+        "\n" +
+        "Whether you're into blockbusters or classics, JetFilms makes it easy to enjoy the world of cinema. Start your journey today! \uD83C\uDFA5"
