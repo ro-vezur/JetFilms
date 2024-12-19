0b1144b8e011bc01fd47b2208daaf78a49dc97bb
ro-vezur <authenttesting@gmail.com>
2024-12-19 23:52:11 +0200

    feat: auth repository

--- app/src/main/java/com/example/jetfilms/Models/Firebase/Auth/AuthService.kt
+++ app/src/main/java/com/example/jetfilms/Models/Firebase/Auth/AuthService.kt
@@ -1,5 +1,6 @@
-package com.example.jetfilms.Models.Firebase
+package com.example.jetfilms.Models.Firebase.Auth
 
+import com.example.jetfilms.Models.Firebase.Auth.Resource
 import com.google.firebase.auth.AuthResult
 import kotlinx.coroutines.flow.Flow
 
@@ -7,4 +8,8 @@ interface AuthService {
     fun loginUser(email: String, password: String): Flow<Resource<AuthResult>>
 
     fun signUp(name: String,email: String,password: String): Flow<Resource<AuthResult>>
+
+    fun createNonce(): String
+
+    fun logInWithGoogle(): Flow<Resource<AuthResult>>
 }
--- app/src/main/java/com/example/jetfilms/Models/Repositories/Firebase/AuthRepository.kt
+++ app/src/main/java/com/example/jetfilms/Models/Repositories/Firebase/AuthRepository.kt
@@ -1,18 +1,31 @@
 package com.example.jetfilms.Models.Repositories.Firebase
 
-import com.example.jetfilms.Models.Firebase.AuthService
-import com.example.jetfilms.Models.Firebase.Resource
+import android.content.Context
+import android.util.Log
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import com.example.jetfilms.Models.Firebase.Auth.AuthService
+import com.example.jetfilms.Models.Firebase.Auth.Resource
+import com.example.jetfilms.R
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
+import java.security.MessageDigest
+import java.util.UUID
 import javax.inject.Inject
 
 class AuthRepository @Inject constructor(
-    private val firebaseAuth: FirebaseAuth
+    private val firebaseAuth: FirebaseAuth,
+    private val context: Context,
 ): AuthService {
     override fun loginUser(email: String, password: String): Flow<Resource<AuthResult>> {
         return flow {
@@ -38,5 +51,88 @@ class AuthRepository @Inject constructor(
        }.catch {
            emit(Resource.Error(message = it.message.toString()))
        }
+    }
+
+    override fun createNonce(): String {
+        val rawNonce = UUID.randomUUID().toString()
+        val bytes = rawNonce.toByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        val digest = md.digest(bytes)
+
+        return digest.fold("") { str,it ->
+            str + "%02x".format(it)
+        }
+    }
+
+    override fun logInWithGoogle(): Flow<Resource<AuthResult>> {
+
+
+        return flow {
+            Log.d("test","test")
+
+            val googleIdOption = GetGoogleIdOption.Builder()
+                .setFilterByAuthorizedAccounts(false)
+                .setServerClientId(context.getString(R.string.web_client_id))
+                .setAutoSelectEnabled(false)
+                .setNonce(createNonce())
+                .build()
+
+            val request = GetCredentialRequest.Builder()
+                .addCredentialOption(googleIdOption)
+                .build()
+
+            Log.d("request",request.toString())
+
+            try {
+                val credentialManager = CredentialManager.create(context)
+                val result = credentialManager.getCredential(
+                    context = context,
+                    request = request
+                )
+
+                val credential = result.credential
+                Log.d("is custom" ,(credential is CustomCredential).toString())
+                if(credential is CustomCredential) {
+                    if(credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+                        try {
+                            val googleIdTokenCredential = GoogleIdTokenCredential
+                                .createFrom(credential.data)
+
+                            val firebaseCredential = GoogleAuthProvider
+                                .getCredential(
+                                    googleIdTokenCredential.idToken,
+                                    null
+                                )
+                            val signInResult = firebaseAuth.signInWithCredential(firebaseCredential)
+
+                            val authResult = signInResult.await()
+
+                            if(signInResult.isSuccessful) {
+                                Log.d("user",authResult.user.toString())
+                                emit(Resource.Success(data = authResult))
+                            } else {
+                                emit(Resource.Error(message = signInResult.exception?.message.toString()))
+                            }
+
+
+                        } catch (e: Exception) {
+                            Log.e("error",e.message.toString())
+                            Resource.Error(
+                                message = e.message.toString(),
+                                data = null
+                            )
+                        }
+                    }
+                }
+            } catch (e:Exception) {
+                Log.e("error",e.message.toString())
+                Resource.Error(
+                    message = e.message.toString(),
+                    data = null
+                )
+            }
+        }.catch {
+          //  emit(Resource.Error(message = it.message.toString()))
+        }
     }
 }
