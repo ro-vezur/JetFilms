b8fdae046876315b176118eef87c4a0600ab2474
ro-vezur <authenttesting@gmail.com>
2024-12-22 20:00:09 +0200

    feat: unified media DTOs

--- app/src/main/java/com/example/jetfilms/Models/DTOs/UnifiedDataPackage/UnifiedMedia.kt
+++ app/src/main/java/com/example/jetfilms/Models/DTOs/UnifiedDataPackage/UnifiedMedia.kt
@@ -1,13 +1,67 @@
 package com.example.jetfilms.Models.DTOs.UnifiedDataPackage
 
+import com.example.jetfilms.Models.DTOs.MoviePackage.DetailedMovieResponse
+import com.example.jetfilms.Models.DTOs.MoviePackage.SimplifiedMovieResponse
+import com.example.jetfilms.Models.DTOs.SeriesPackage.DetailedSeriesResponse
+import com.example.jetfilms.Models.DTOs.SeriesPackage.SimplifiedSeriesResponse
 import com.example.jetfilms.View.Screens.Start.Select_type.MediaCategories
 
 data class UnifiedMedia(
     val id: Int,
-    val name: String,
+    val title: String,
     val poster: String,
     val releaseDate: String,
     val rating: Float,
     val popularity: Float,
-    val mediaType: MediaCategories
-)
+    val mediaCategory: MediaCategories
+) {
+    companion object {
+        fun fromDetailedMovieResponse(movie: DetailedMovieResponse): UnifiedMedia {
+            return UnifiedMedia(
+                id = movie.id,
+                title = movie.title,
+                poster = movie.posterUrl,
+                mediaCategory = MediaCategories.MOVIE,
+                popularity = movie.popularity,
+                rating = movie.rating,
+                releaseDate = movie.releaseDate,
+            )
+        }
+
+        fun fromDetailedSeriesResponse(series: DetailedSeriesResponse): UnifiedMedia {
+            return UnifiedMedia(
+                id = series.id,
+                title = series.title,
+                poster = series.poster.toString(),
+                mediaCategory = MediaCategories.SERIES,
+                popularity = series.popularity,
+                rating = series.rating,
+                releaseDate = series.releaseDate
+            )
+        }
+
+        fun fromSimplifiedMovieResponse(movie: SimplifiedMovieResponse): UnifiedMedia {
+            return UnifiedMedia(
+                id = movie.id,
+                title = movie.title,
+                poster = movie.posterUrl,
+                mediaCategory = MediaCategories.MOVIE,
+                popularity = movie.popularity,
+                rating = movie.rating,
+                releaseDate = movie.releaseDate,
+            )
+        }
+
+        fun fromSimplifiedSeriesResponse(series: SimplifiedSeriesResponse): UnifiedMedia {
+            return UnifiedMedia(
+                id = series.id,
+                title = series.name,
+                poster = series.poster.toString(),
+                mediaCategory = MediaCategories.SERIES,
+                popularity = series.popularity,
+                rating = series.rating,
+                releaseDate = series.releaseDate
+            )
+        }
+    }
+}
