6227cd495febe6c7656c6a71a115d11e3b1c1844
ro-vezur <authenttesting@gmail.com>
2024-12-22 18:51:07 +0200

    feat: gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 buildscript {
     dependencies {
         classpath(libs.google.services)
+        classpath("com.android.tools.build:gradle:8.3.0")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
