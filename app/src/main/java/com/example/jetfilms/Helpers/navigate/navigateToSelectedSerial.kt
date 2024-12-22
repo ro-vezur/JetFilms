471fc0b55cfc53a220f17839cc5346d65e6f022a
ro-vezur <authenttesting@gmail.com>
2024-12-22 19:50:44 +0200

    feat: navigate to detailed media

--- app/src/main/java/com/example/jetfilms/Helpers/navigate/navigateToSelectedSerial.kt
+++ app/src/main/java/com/example/jetfilms/Helpers/navigate/navigateToSelectedSerial.kt
@@ -1,17 +1,17 @@
 package com.example.jetfilms.Helpers.navigate
 
 import androidx.navigation.NavController
-import com.example.jetfilms.Models.DTOs.SeriesPackage.DetailedSerialResponse
+import com.example.jetfilms.Models.DTOs.SeriesPackage.DetailedSeriesResponse
 import com.example.jetfilms.Helpers.encodes.encodeStringWithSpecialCharacter
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
-fun navigateToSelectedSerial(navController: NavController, selectedSerial: DetailedSerialResponse){
+fun navigateToSelectedSerial(navController: NavController, selectedSerial: DetailedSeriesResponse){
 
     val jsonSerial = Json.encodeToString(
         selectedSerial.copy(
             overview = encodeStringWithSpecialCharacter(selectedSerial.overview),
-            name = encodeStringWithSpecialCharacter(selectedSerial.name),
+            title = encodeStringWithSpecialCharacter(selectedSerial.title),
             poster = encodeStringWithSpecialCharacter(selectedSerial.poster.toString()),
         )
     )
